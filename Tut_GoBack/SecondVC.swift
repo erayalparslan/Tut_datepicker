2b5e36c6c153699bae0da44365dd3e4fec01047f
Eray Alparslan <erayalparslan@yahoo.com>
2018-09-09 12:37:31 +0300

    iOS Notlari

---
+++ Tut_GoBack/SecondVC.swift
@@ -0,0 +1,20 @@
+//
+//  SecondVC.swift
+//  Tut_GoBack
+//
+//  Created by ruroot on 9/5/18.
+//  Copyright © 2018 Eray Alparslan. All rights reserved.
+//
+
+import UIKit
+
+class SecondVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
--- Tut_GoBack/ViewController.swift
+++ Tut_GoBack/ViewController.swift
@@ -13,11 +13,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print("iOS Notları'ndan merhaba!")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+  
+    @IBAction func unwindToFirst(_segue: UIStoryboardSegue){
+        
     }
 
 
